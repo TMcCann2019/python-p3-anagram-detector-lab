1762c8998050d5a91e9ab2e38d1097ac444bef2c
Tim McCann <timothysmccann@gmail.com>
2024-01-31 09:42:31 -0500

    completed lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
 # your code goes here!
+
+class Anagram():
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, possible_anagrams):
+        return [anagram for anagram in possible_anagrams if self._is_anagram(anagram)]
+
+    def _is_anagram(self, candidate):
+        candidate_lower = candidate.lower()
+        return candidate_lower != self.word and sorted(candidate_lower) == sorted(self.word)
